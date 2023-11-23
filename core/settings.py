bc073f733cb027da4dcba1fde4cd1e33cbf0afcb
BwanaQ <thunjawax@gmail.com>
2023-11-23 12:00:29 +0300

    comment out sqlite3 from settings.py

--- core/settings.py
+++ core/settings.py
@@ -109,12 +109,12 @@ DATABASES = {
     }
 }
 
-if not DATABASES['default']['NAME']:
-    # Fallback to SQLite if no PostgreSQL settings provided
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+# if not DATABASES['default']['NAME']:
+#     # Fallback to SQLite if no PostgreSQL settings provided
+#     DATABASES['default'] = {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
