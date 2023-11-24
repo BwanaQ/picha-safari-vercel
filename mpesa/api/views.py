9175a35d97d3894c3346814f1441f11449a0efdb
BwanaQ <thunjawax@gmail.com>
2023-11-24 10:59:30 +0300

    allowany to callback URL

--- mpesa/api/views.py
+++ mpesa/api/views.py
@@ -1,12 +1,12 @@
 from mpesa.models import LNMOnline
 from mpesa.api.serializers import LNMOnlineSerializer
 from rest_framework.generics import CreateAPIView
-# from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import AllowAny
 
 class LNMCallbackUrlAPIView(CreateAPIView):
     queryset = LNMOnline.objects.all()
     serializer_class = LNMOnlineSerializer
-    # permission_classes = [IsAdminUser]
+    permission_classes = [AllowAny]
 
     def create(self,request):
         print(request.data,"This is request.data")
