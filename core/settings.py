8e2950885905f382269d52096aab1da046c41c89
BwanaQ <thunjawax@gmail.com>
2024-02-01 11:29:22 +0300

    revert to dot notation for allowed hosts for vercel

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','picha-safari-vercel.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.vercel.app','.now.sh']
 
 
 # Application definition
