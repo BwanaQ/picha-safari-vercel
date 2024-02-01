89466cec80a7f48f7fca4a42d051ee020184b819
BwanaQ <thunjawax@gmail.com>
2024-02-01 10:31:48 +0300

    fix views briefly

--- mpesa/api/views.py
+++ mpesa/api/views.py
@@ -16,45 +16,45 @@ class LNMCallbackUrlAPIView(CreateAPIView):
     def create(self, request):
         print(f"this is request.data=======>{request.data}")
 
-        if request.user.is_authenticated:
-            cart, created = Cart.objects.get_or_create(user=request.user, completed=False)
-        cart = cart
-        merchant_request_id = request.data["Body"]["stkCallback"]["MerchantRequestID"]
-        checkout_request_id = request.data["Body"]["stkCallback"]["CheckoutRequestID"]
-        result_code = request.data["Body"]["stkCallback"]["ResultCode"]
-        result_description = request.data["Body"]["stkCallback"]["ResultDesc"]
-        amount = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][0]["Value"]
-        mpesa_receipt_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][1]["Value"]
-        balance = ""
-        transaction_date = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][3]["Value"]
-        phone_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][4]["Value"]
-        str_transaction_date = str(transaction_date)
-        transaction_datetime = datetime.strptime(str_transaction_date, "%Y%m%d%H%M%S")
-        aware_transaction_datetime = pytz.utc.localize(transaction_datetime)
+        # if request.user.is_authenticated:
+        #     cart, created = Cart.objects.get_or_create(user=request.user, completed=False)
+        # cart = cart
+        # merchant_request_id = request.data["Body"]["stkCallback"]["MerchantRequestID"]
+        # checkout_request_id = request.data["Body"]["stkCallback"]["CheckoutRequestID"]
+        # result_code = request.data["Body"]["stkCallback"]["ResultCode"]
+        # result_description = request.data["Body"]["stkCallback"]["ResultDesc"]
+        # amount = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][0]["Value"]
+        # mpesa_receipt_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][1]["Value"]
+        # balance = ""
+        # transaction_date = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][3]["Value"]
+        # phone_number = request.data["Body"]["stkCallback"]["CallbackMetadata"]["Item"][4]["Value"]
+        # str_transaction_date = str(transaction_date)
+        # transaction_datetime = datetime.strptime(str_transaction_date, "%Y%m%d%H%M%S")
+        # aware_transaction_datetime = pytz.utc.localize(transaction_datetime)
 
-        our_model = LNMOnline.objects.create(
-            cart=cart,
-            CheckoutRequestID=checkout_request_id,
-            MerchantRequestID=merchant_request_id,
-            Amount=amount,
-            ResultCode=result_code,
-            ResultDesc=result_description,
-            MpesaReceiptNumber=mpesa_receipt_number,
-            Balance=balance,
-            TransactionDate=aware_transaction_datetime,
-            PhoneNumber=phone_number,
-        )
-        our_model.save()
-        if result_code == "0":
-            # Set cart.completed to true
-            cart.completed = True
-            cart.save()
+        # our_model = LNMOnline.objects.create(
+        #     cart=cart,
+        #     CheckoutRequestID=checkout_request_id,
+        #     MerchantRequestID=merchant_request_id,
+        #     Amount=amount,
+        #     ResultCode=result_code,
+        #     ResultDesc=result_description,
+        #     MpesaReceiptNumber=mpesa_receipt_number,
+        #     Balance=balance,
+        #     TransactionDate=aware_transaction_datetime,
+        #     PhoneNumber=phone_number,
+        # )
+        # our_model.save()
+        # if result_code == "0":
+        #     # Set cart.completed to true
+        #     cart.completed = True
+        #     cart.save()
 
-            # Redirect to the home page with a success message
-            messages.success(request, 'Checkout was successful!')
-            return redirect('cart-home')  # Replace 'home' with the actual URL name or path of your home page
+        #     # Redirect to the home page with a success message
+        #     messages.success(request, 'Checkout was successful!')
+        #     return redirect('cart-home')  # Replace 'home' with the actual URL name or path of your home page
 
-        else:
-            # Return to home page with a failure message
-            messages.error(request, 'Checkout was not successful.')
-            return redirect('cart-home')  # Replace 'home' with the actual URL name or path of your home page
+        # else:
+        #     # Return to home page with a failure message
+        #     messages.error(request, 'Checkout was not successful.')
+        #     return redirect('cart-home')  # Replace 'home' with the actual URL name or path of your home page
