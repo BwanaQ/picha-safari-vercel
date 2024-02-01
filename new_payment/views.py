bbc8b4b2847e827aeeacc3016b3211a0758de919
BwanaQ <thunjawax@gmail.com>
2024-02-01 13:36:45 +0300

    change callback endpoint

--- new_payment/views.py
+++ new_payment/views.py
@@ -10,12 +10,12 @@ def index(request):
     amount = 1
     account_reference = 'reference'
     transaction_desc = 'Description'
-    callback_url = 'https://darajambili.herokuapp.com/express-payment';
-    # callback_url = 'https://picha-safari-vercel.vercel.app/new-payment/daraja/stk_push/'
+    # callback_url = 'https://darajambili.herokuapp.com/express-payment';
+    callback_url = 'https://picha-safari-vercel.vercel.app/new-payment/daraja/stk_push/'
     response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
     return HttpResponse(response)
 
 def stk_push_callback(request):
         data = request.body
-        
+        print(data)
         return HttpResponse(data)
