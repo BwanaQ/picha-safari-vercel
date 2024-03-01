1c2d922425182c1d7dc02ec1a0dbd8799d6eb5ef
BwanaQ <thunjawax@gmail.com>
2024-03-01 16:19:49 +0300

    set up paypal IPN and migrations

--- core/urls.py
+++ core/urls.py
@@ -32,6 +32,9 @@ urlpatterns = [
     path('payment/', include('payment.urls')),
     # photo API
     path('api/v1/', include('photo.api.urls')),
+    # paypal ipn urls
+    path('paypal', include('paypal.standard.ipn.urls')),
+
 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
