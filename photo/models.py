0423f1725ef8a4cd6a505a7d0cc58750a812b6fb
BwanaQ <thunjawax@gmail.com>
2024-03-24 10:46:05 +0300

    remove duplicate photo model

--- photo/models.py
+++ photo/models.py
@@ -31,19 +31,6 @@ class Photo(Timestamp):
     def __str__(self):
         return f"{self.title} @ KES {self.price}"
 
-class Photo(Timestamp):
-    title = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
-    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
-    image = CloudinaryField('image')
-    webp_image = models.URLField(blank=True)  # Change to URLField to store the URL
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='photos', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.title} @ KES {self.price}"
-
     def save(self, *args, **kwargs):
         # Convert the original image to WebP format and save the URL
         if self.image and not self.webp_image:
