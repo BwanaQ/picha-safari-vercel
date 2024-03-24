0e1618da0548e0a9c43e0724d3815740f91c3236
BwanaQ <thunjawax@gmail.com>
2024-03-24 10:43:31 +0300

    bf: photo.models.py

--- photo/models.py
+++ photo/models.py
@@ -4,7 +4,7 @@ from cloudinary.models import CloudinaryField
 from django.conf import settings
 import cloudinary.uploader
 import uuid
-
+import os
 
 class Tag(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
@@ -31,7 +31,20 @@ class Photo(Timestamp):
     def __str__(self):
         return f"{self.title} @ KES {self.price}"
 
-def save(self, *args, **kwargs):
+class Photo(Timestamp):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+    image = CloudinaryField('image')
+    webp_image = models.URLField(blank=True)  # Change to URLField to store the URL
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='photos', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} @ KES {self.price}"
+
+    def save(self, *args, **kwargs):
         # Convert the original image to WebP format and save the URL
         if self.image and not self.webp_image:
             # Upload the original image to Cloudinary
@@ -55,3 +68,4 @@ def save(self, *args, **kwargs):
             cloudinary.uploader.upload(self.image.file, public_id=webp_filename, format="webp")
 
         super().save(*args, **kwargs)
+
